f692f17fdf668abd6381804d9afc43b65eb8a680
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-04-03 14:54:01 +0200

    Add anagrams solution

---
+++ module1/anagrams.js
@@ -0,0 +1,23 @@
+function anagramCounter (wordsArray) {
+  var anagramCount =
+    wordsArray.reduce(function (count, thisWord, index) {
+      var restOfWords = wordsArray.slice(index + 1);
+      var anagramsForThisWord =
+        restOfWords.filter(function (oneWord) {
+          return isAnagram(thisWord, oneWord);
+        });
+      return count + anagramsForThisWord.length;
+    }, 0);
+
+  return anagramCount;
+}
+
+function isAnagram (wordA, wordB) {
+  var lettersA = [...wordA];
+  var lettersB = [...wordB];
+
+  lettersA.sort();
+  lettersB.sort();
+
+  return lettersA.join('') === lettersB.join('');
+}
