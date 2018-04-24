23f3577c28ade1ad0f0683380244ca49ece72e7c
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-04-24 10:10:16 +0200

    Add persistence solution

---
+++ module2/persistence.js
@@ -0,0 +1,22 @@
+function persistence (num) {
+  let strNum = String(num);
+  let persistence = 0;
+
+  while (strNum.length !== 1) {
+    num = multiplyDigits(strNum);
+    strNum = String(num);
+    persistence += 1;
+  }
+
+  return persistence;
+}
+
+function multiplyDigits (strNum) {
+  const digits = [...strNum];
+  const result =
+    digits.reduce((mult, oneDigit) => {
+      return mult * oneDigit;
+    }, 1);
+
+  return result;
+}
