852899d85e17969f9041c904a202232aee44afc7
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-04-24 10:45:42 +0200

    Add missing letter solution

---
+++ module2/missing-letter.js
@@ -0,0 +1,13 @@
+function findMissingLetter (array) {
+  const allButLast = array.slice(0, -1);
+
+  const prevLetter =
+    allButLast.find((oneLetter, index) => {
+      const currentCode = oneLetter.charCodeAt(0);
+      const nextCode = array[index + 1].charCodeAt(0);
+      return nextCode !== currentCode + 1;
+    });
+
+  const prevCode = prevLetter.charCodeAt(0);
+  return String.fromCharCode(prevCode + 1);
+}
