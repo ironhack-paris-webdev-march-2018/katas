554da4c022d58d3d194840e3155dd8b6feb0ff9f
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-05-08 15:22:19 +0200

    Add directions solition

---
+++ module3/directions.js
@@ -0,0 +1,23 @@
+function dirReduc (arr) {
+  const simpleDirections = [];
+
+  arr.forEach((dir) => {
+    const previousDir = simpleDirections[simpleDirections.length - 1];
+    if (previousDir === opposites[dir]) {
+      // remove the last direction if this direction cancels it
+      simpleDirections.pop();
+    }
+    else {
+      simpleDirections.push(dir);
+    }
+  });
+
+  return simpleDirections;
+}
+
+const opposites = {
+  NORTH: 'SOUTH',
+  SOUTH: 'NORTH',
+  EAST: 'WEST',
+  WEST: 'EAST'
+};
