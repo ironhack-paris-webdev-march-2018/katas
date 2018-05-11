9ff94b124a354f78a76317d2cab791b021e2302a
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-05-11 12:49:55 +0200

    Add cake recipe solution

---
+++ module3/cake-recipe.js
@@ -0,0 +1,15 @@
+function cakes (recipe, available) {
+  // How many cakes could we make with each ingredient?
+  const capacities =
+    // For every ingredient...
+    Object.keys(recipe).map((ing) => {
+      if (!available[ing]) {
+        return 0;
+      }
+      // Divide the available amount by what the recipe needs (rounded down)
+      return Math.floor(available[ing] / recipe[ing]);
+    });
+
+  // Return the smallest amount of cakes we could make
+  return Math.min(...capacities);
+}
